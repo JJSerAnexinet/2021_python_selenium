ec4433a6b0b13fba977fddf3d363ea45b1cce9f6
JJ Serna <jj.sernav@hotmail.com>
2021-03-30 18:26:58 -0600

    Browsing file

--- Module_02/browsing.py
+++ Module_02/browsing.py
@@ -3,6 +3,8 @@
 from pathlib import Path
 from selenium import webdriver
 
+# from module_02.browsing import get_chrome_path -> jala el metodo para el chromedriver a otro archivo
+
 
 def get_project_root() -> Path:
     """Get project root path."""
@@ -16,22 +18,44 @@ def get_chrome_path() -> Path:
     root = get_project_root()
     return root.joinpath('drivers','chromedriver')
 
+def get_firefox_path() -> Path:
+    """Get Firefox driver path."""
+    #1. Root = path del chrome driver
+    #2. une ambos path "proyecto + chrome"
+    root = get_project_root()
+    return root.joinpath('drivers','geckodriver')
+
+def print_page_details(driver: webdriver.Remote):
+    """Get page details, title, currentURL, code"""
+    print(f'Current title: {driver.title}')
+    print(f'Current url: {driver.current_url}')
+
 driver = webdriver.Chrome(executable_path=get_chrome_path())
 
 driver.get('https://www.google.com')
-print(f'Current title: {driver.title}')
-print(f'Current url: {driver.current_url}')
+print_page_details(driver)
 
 driver.get('https://www.mlb.com/es')
-print(f'Current title: {driver.title}')
-print(f'Current url: {driver.current_url}')
+print_page_details(driver)
 print(f'Current source: {driver.page_source}')
 
 driver.get('https://www.nytimes.com/es')
 driver.refresh()
+print_page_details(driver)
+
+driver.back()
+
+driver.back()
 print(f'Current title: {driver.title}')
 print(f'Current url: {driver.current_url}')
-driver.back()
-driver.back()
-print(f'Current title: {driver.title}')
-print(f'Current url: {driver.current_url}')
+
+print(driver.get_cookies())
+print(driver.application_cache)
+
+if "SOCzOAOac8uhByk5zGU2Zg==" in driver.page_source:
+    print("Found")
+else:
+    print("Not Found")
+
+driver.quit()
+
