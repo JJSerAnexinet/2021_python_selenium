f8b293e66187a7faffba5e82ec1bf6c986b9a90e
JJ Serna <jj.sernav@hotmail.com>
2021-05-14 15:39:20 -0500

    This contains the test base

---
+++ Module_06/tests/test_google.py
@@ -0,0 +1,16 @@
+from common.webdriver_factory import get_driver
+from Module_06.src.pages.google import Google
+
+
+def test_google_search():
+    driver = get_driver('chrome')
+    page = Google(driver)
+    page.open()
+    page.search('Selenium')
+
+
+def test_feeling_lucky():
+    driver = get_driver('chrome')
+    page = Google(driver)
+    page.open()
+    page.feeling_lucky('Selenium')
